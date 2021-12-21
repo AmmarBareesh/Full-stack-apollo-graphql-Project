722f56de7723d6ace167eb3169f1ef97990ea423
Ammar_Bareesh <bareesh.ammar00@gmail.com>
2021-12-21 15:45:53 +0300

    first commit

---
+++ client/src/components/header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>Astroo</h1>
+        </div>
+    )
+}
+
+export default Header
---
+++ server/db.js
@@ -0,0 +1,57 @@
+const db = [
+    {
+      id: "5xc1",
+      username: "Astroo",
+      email: "astroo@email.com",
+      password: "abc123",
+      records: [
+        {
+          id: "usc000lv5e",
+          magnitude: 5.1,
+          location: "76km NNW of Davila, Philippines",
+          when: "January 1, 2014 at 9:01 and 16 seconds"
+        },
+        {
+          id: "usc000lv5j",
+          magnitude: 4.5,
+          location: "163km W of Neiafu, Tonga",
+          when: "January 1, 2014 at 9:05 and 28 seconds"
+        },
+        {
+          id: "uw60664271",
+          magnitude: 2.2,
+          location: "1km NW of Sweet Home, Oregon",
+          when: "January 1, 2014 at 9:05 and 55 seconds"
+        },
+        {
+          id: "nc72133841",
+          magnitude: 0.57,
+          location: "9km WNW of The Geysers, California",
+          when: "January 1, 2014 at 9:10 and 10 seconds"
+        }
+      ]
+    },
+    {
+      id: "5xc2",
+      username: "Astroo2",
+      email: "astroo2@email.com",
+      password: "abss1233",
+      records: [
+        {
+          id: "usc000lv5e",
+          magnitude: 5.1,
+          location: "76km NNW of Davila, Philippines",
+          when: "January 1, 2014 at 9:01 and 16 seconds"
+        }
+      ]
+    },
+    {
+      id: "5xc3",
+      username: "Astroo3",
+      email: "astroo3@zmail.com",
+      password: "ammar123",
+      records: []
+    }
+  ];
+  
+  module.exports = db;
